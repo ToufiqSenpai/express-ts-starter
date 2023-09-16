4323d020da1b16228aa480d75f46b37a6cdc7aaf
Muhammad Taufiqurrahman <mhmtaufiqurrahman@gmail.com>
2023-09-16 18:18:49 +0700

    Remove BodyBuilder util

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express"
-import BodyBuilder from "../utils/BodyBuilder"
 import HttpStatus from "../enums/HttpStatus"
 
 namespace HomeController {
   export function get(req: Request, res: Response) {
-    return res.status(200).json(new BodyBuilder(HttpStatus.OK, 'Hello World'))
+    return res.status(200).json({ message: 'Ok' })
   }
 }
 
